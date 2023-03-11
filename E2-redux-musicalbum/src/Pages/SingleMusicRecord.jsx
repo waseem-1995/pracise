6de871a230ab9180ac7f8e425b9ff479dd50886d
waseem <iwaseemkhan1995@gmail.com>
2023-03-11 18:46:30 +0530

    aa

--- E2-redux-musicalbum/src/Pages/SingleMusicRecord.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SingleMusicRecord = () => {
-  return (
-    <div>SingleMusicRecord</div>
-  )
-}
--- Week-2/day1_thunk_classcode/src/Pages/SingleTodo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingleTodo = () => {
-  return (
-    <div>SingleTodo</div>
-  )
-}
-
-export default SingleTodo
